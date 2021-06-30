dbb251600c6cfb1a89a5deab106e8c2123106700
vamsikrishna <39961654+vamsi973@users.noreply.github.com>
2021-06-30 23:40:23 +0530

    setting up
    setting up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StartingComponent } from './starting/starting.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, StartingComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
