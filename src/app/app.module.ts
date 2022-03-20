241eab6a5f02ec7092554ad4225df54bbc14e5b8
harish973 <danda.harishbabu@gmail.com>
2022-03-20 16:44:19 +0530

    Revert "setting up"
    This reverts commit dbb251600c6cfb1a89a5deab106e8c2123106700.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { StartingComponent } from './starting/starting.component';
 
 @NgModule({
-  declarations: [AppComponent, StartingComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
